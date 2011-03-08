bbeeee0c7a4a2312accc426181bcf3e4f6db8b5d
Torsten- <torsten@amshove.net>
2011-03-08 21:19:46 +0000

    cronjob

---
+++ webinterface/cronjob/del_rules.php
@@ -0,0 +1,12 @@
+<?php
+$path = substr(dirname($_SERVER["SCRIPT_FILENAME"]),0,-8);
+require($path."/config.inc.php");
+require($path."/functions.inc.php");
+
+$query = mysql_query("SELECT id, ip FROM history WHERE active = 1 AND end_date > 0 AND end_date < '".time()."'");
+while($row = mysql_fetch_assoc($query)){
+  if(iptables_del($row["ip"])){
+    mysql_query("UPDATE history SET active = 0, del_user = 'Cronjob', del_date = '".time()."' WHERE id = '".$row["id"]."' LIMIT 1");
+  }
+}
+?>
